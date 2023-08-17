5440713219052ff86860b9ce41b0e34dd86f29d9
Moeenuddin <34792583+Moeenuddin178@users.noreply.github.com>
2023-08-17 23:13:03 +0500

    Mvvm With jetpack compose

---
+++ app/src/main/java/com/example/mvvmjetpack07/product/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.example.mvvmjetpack07.product
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mvvmjetpack07.ui.theme.MVVMJETPACK07Theme
+
+@Composable
+fun HomeFragment(modifier: Modifier) {
+    Surface (
+        modifier = modifier.fillMaxSize(),
+        ){
+        LazyVerticalGrid(
+            modifier = Modifier.padding(16.dp),
+            columns = GridCells.Adaptive(minSize = 96.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(60) {
+                ProductCard(modifier)
+            }
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    MVVMJETPACK07Theme {
+        HomeFragment(modifier = Modifier)
+    }
+}
